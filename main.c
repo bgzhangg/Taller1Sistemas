d32655df82e216acbaf4316b1d9c0e1c34314663
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2024-11-04 10:47:28 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,58 @@
 #include "headers/datos.h"
 int main(){
-    mostrar_menu_figuras();
-    int valorIngresado=pedir_opcion();
-    int bandera=1;
-    mostrar_menu_figuras();
-    // Verificar que sea una de las opciones
-    float valor=0.0;
-    while (bandera){
+    int valorIngresado=0;
+    int seguir=1;
+    while (seguir){
+        mostrar_menu_figuras();
+        valorIngresado=pedir_opcion();
         switch (valorIngresado)
         {
         case 1:
             triangulo();
-            bandera=0;
             break;
         case 2:
             paralelogramo();
-            bandera=0;
             break;
         case 3:
             cuadrado();
-            bandera=0;
             break;
         case 4:
             rectangulo();
-            bandera=0;
             break;
         case 5:
             rombo();
-            bandera=0;
             break;
         case 6:
             trapecio();
-            bandera=0;
             break;
         case 7:
             circulo();
-            bandera=0;
             break;
         case 8:
             poligono_regular();
-            bandera=0;
+            break;
+        case 9:
+            cubo();
+            break;
+        case 10:
+            cuboide();
+            break;
+        case 11:
+            cilindro();
+            break;
+        case 12:
+            esfera();
+            break;
+        case 13:
+            cono();
+            break;
+        case 14:
+            //Salir
+            seguir=0;
             break;
         default:
+            // Verificar que sea una de las opciones
             repito();
-            valorIngresado=pedir_opcion();
             break;
         }
     }
