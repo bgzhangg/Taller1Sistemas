fbee1ad810561ab2a03ce587d6f84032ec478073
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2024-11-03 23:53:59 -0500

    Create calculos.h

---
+++ headers/calculos.h
@@ -0,0 +1,13 @@
+#ifndef calculos
+#define calculos
+float area_triangulo(float base, float altura);
+float perimetro_triangulo(float lado1,float lado2, float lado3);
+float perimetro_cuadrilatero(float lado1,float lado2);
+float area_cuadrilatero(float base, float altura);
+float area_trapecio(float basemayor,float basemenor,float altura);
+float perimetro_trapecio(float basemayor,float basemenor, float lado1, float lado2);
+float area_circulo(float radio);
+float perimetro_circulo(float radio);
+float perimetro_poligonor(float lado, float  n);
+float area_poligonor(float lado, float n);
+#endif
