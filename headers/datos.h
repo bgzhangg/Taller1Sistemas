9bfaf79c5239713a7b8321422a58b2b3c8cc2927
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2024-11-04 00:00:51 -0500

    Datos header

---
+++ headers/datos.h
@@ -0,0 +1,14 @@
+#ifndef datos
+#define datos
+void mostrar_menu_figuras();
+int pedir_opcion();
+void repito();
+void triangulo(float l1, float l2,float l3, float base, float altura);
+void paralelogramo(float a, float b, float base, float altura);
+void cuadrado(float lado);
+void rectangulo(float base,float altura);
+void rombo(float diagonalM,float diagonalm,float lado);
+void trapecio(float lado1,float lado2,float basem,float baseM,float altura);
+void circulo(float radio);
+void poligono_regular(float lado,float n);
+#endif
